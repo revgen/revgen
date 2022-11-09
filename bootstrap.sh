703d390199e129c9ce293d73c85573b5f60aa1fd
Evgen Rusakov <revgen@users.noreply.github.com>
2022-11-08 22:04:55 -0500

    Save

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+if [ -z "${GITPOD_WORKSPACE_ID}" ]; then
+    echo "Error: the script is setup environemnt for Gitpod Workspace"
+    echo "Looks like it is not a Gitpod Workspace environment. Skip."
+    exit 1
+fi
+
 TMPDIR="$(mktemp -d)"
 CURRENT="${PWD}"
 cd "${TMPDIR}"
