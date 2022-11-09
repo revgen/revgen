06d47a4c8ed132999564ca97a70ee91b258e6855
Evgen Rusakov <revgen@users.noreply.github.com>
2022-11-08 22:02:31 -0500

    Add dotfiles for gitpod

---
+++ bootstrap.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+TMPDIR="$(mktemp -d)"
+CURRENT="${PWD}"
+cd "${TMPDIR}"
+
+for script in ~/.dotfiles/scripts/gitpod/*; do
+  bash "${script}"
+done
+
+cd "${CURRENT}"
+rm -rf "${TMPDIR}"
