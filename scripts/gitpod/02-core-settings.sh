9cb0ee9b15fb9a4dd44ec749b5f83b05ddd4f80e
Evgen <revgen@users.noreply.github.com>
2023-07-11 21:02:58 -0400

    Remove unused files

--- scripts/gitpod/02-core-settings.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-MYENV_REPO=https://raw.githubusercontent.com/revgen/myenv
-
-wget -O ~/.gitconfig    "${MYENV_REPO}/master/home/.gitconfig"
-wget -O ~/.pylintrc     "${MYENV_REPO}/master/home/.pylintrc"
-wget -O ~/.vimrc        "${MYENV_REPO}/master/home/.vimrc"
